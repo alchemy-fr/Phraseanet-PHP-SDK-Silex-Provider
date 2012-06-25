56fff59f991c84e35950ae57dfa73f8450815e45
Romain Neutron <imprec@gmail.com>
2012-06-25 16:37:28 +0200

    Fix load

--- src/Alchemy/Phrasea/SDK/PhraseanetSDKServiceProvider.php
+++ src/Alchemy/Phrasea/SDK/PhraseanetSDKServiceProvider.php
@@ -24,23 +24,10 @@ class PhraseanetSDKServiceProvider implements ServiceProviderInterface
 
     public function register(Application $app)
     {
-        if ( ! isset($app['phraseanet-sdk.apiSecret'])) {
-            throw new RuntimeException('You must provide an api secret');
-        }
-
-        if ( ! isset($app['phraseanet-sdk.apiKey'])) {
-            throw new RuntimeException('You must provide an api key');
-        }
-
-        if ( ! isset($app['phraseanet-sdk.apiUrl'])) {
-            throw new RuntimeException('You must provide an api url');
-        }
-
-        $app['phraseanet-sdk'] = $app->share(function() {
+        $app['phraseanet-sdk'] = $app->share(function() use ($app) {
 
                 $guzzle = $app['guzzle.client'];
                 $guzzle->setBaseUrl($app['phraseanet-sdk.apiUrl']);
-
 
                 $client = new Client($app['phraseanet-sdk.apiKey'], $app['phraseanet-sdk.apiSecret'], $guzzle, $app['monolog']);
 
@@ -54,6 +41,17 @@ class PhraseanetSDKServiceProvider implements ServiceProviderInterface
 
     public function boot(Application $app)
     {
+        if ( ! isset($app['phraseanet-sdk.apiSecret'])) {
+            throw new RuntimeException('You must provide an api secret');
+        }
+
+        if ( ! isset($app['phraseanet-sdk.apiKey'])) {
+            throw new RuntimeException('You must provide an api key');
+        }
+
+        if ( ! isset($app['phraseanet-sdk.apiUrl'])) {
+            throw new RuntimeException('You must provide an api url');
+        }
 
         if ( ! isset($app['monolog'])) {
             throw new RuntimeException('Phraseanet SDK Provider requires monolog service');
